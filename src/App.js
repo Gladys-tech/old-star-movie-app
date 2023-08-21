12c5b34538d04540b2cd7fa21632c54633e27946
Gladys-tech <gladyskyambadde0@gmail.com>
2023-08-21 16:59:55 +0300

    search API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from "./search.svg";
+import MovieCard from './MovieCard';
+
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4edbae7a"
 
 function App() {
+
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState('');
+
+  const searchMovies = async (tittle) => {
+    const response = await fetch(`${API_URL}&s=${tittle}`);
+    const data = await response.json();
+
+    //console.log(data.Search);
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('superman')
+  }, [])
+
+  // const movie1 = {
+  //   Poster
+  //     :
+  //     "N/A",
+  //"https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  // Title
+  //   :
+  //   "Batman v Superman: Dawn of Justice",
+  // Type
+  //   :
+  //   "movie",
+  // Year
+  //   :
+  //   "2016",
+  //   imdbID
+  //     :
+  //     "tt2975590",
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/*  4edbae7a */}
+      {/*  http://www.omdbapi.com/?i=tt3896198&apikey=4edbae7a */}
+      <h1>Flick App</h1>
+      <div className='search'>
+        <input
+          placeholder='search for old star movies'
+          value={searchTerm}
+          onChange={(e) => setsearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+
+          onClick={() => searchMovies(searchTerm)}
+        />
+
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className='container'>
+              {/* <MovieCard movie1={movie1}/> */}
+              {movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>no movies found</h2>
+            </div>
+          )
+      }
+
+
     </div>
   );
 }
